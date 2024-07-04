427ceb069dfb3be9ada2a927cd830afffb833679
CYsky <yuc@outlook.my>
2024-07-04 16:43:23 +0800

    a24f5e1
    更新a24f5e1

--- src/components/basic/Drawer.vue
+++ src/components/basic/Drawer.vue
@@ -64,8 +64,8 @@ function onDrawerClick(e: Event) {
         </div>
         <div class="drawer__footer">
           <sketch-outlined class="footer-icon" />
-          <a href="https://github.com/EsunR/Blog-Index" target="_blank">
-            Driven by <b>EsunR/Blog-Index</b>
+          <a href="https://chenyu.me" target="_blank">
+            Driven by <b>ChenYu.Me</b>
           </a>
         </div>
       </div>
--- src/config.ts
+++ src/config.ts
@@ -1,48 +1,241 @@
 /** Website 分类配置 */
 const WEBSITE_SORTS: WebsiteSort[] = [
   {
-    title: "示例分类1",
+    title: "周易",
     sites: [
       {
-        title: "主标题",
-        url: "http://www.your-web-page.com/",
+        title: "尘语",
+        description: "万物皆语言",
+        url: "https://my.chenyu.me/cy/",
         icon: "",
         color: "#0171CD",
       },
       {
-        title: "主标题",
-        description: "网站描述",
-        url: "http://www.your-web-page.com/",
+        title: "奇语",
+        description: "万物加密成尘遇",
+        url: "https://my.chenyu.me/jiami/",
         icon: "",
       },
       {
-        title: "主标题",
-        description: "网站描述",
-        url: "http://www.your-web-page.com/",
+        title: "价值观",
+        description: "加密成价值观",
+        url: "https://my.chenyu.me/jzg/",
+        icon: "",
+      },
+      {
+        title: "打分",
+        description: "工作打分",
+        url: "https://my.chenyu.me/gzdf/",
+        icon: "",
+      },
+      {
+        title: "遇上你",
+        description: "好久不见",
+        url: "https://my.chenyu.me/daima/ysni/",
+        icon: "",
+      },
+      {
+        title: "足迹",
+        description: "在路上",
+        url: "https://my.chenyu.me/map/",
+        icon: "",
+      },
+      {
+        title: "路过",
+        description: "路过人间",
+        url: "https://my.chenyu.me/ditu/",
+        icon: "",
+      },
+      {
+        title: "收款码",
+        description: "三合一收款码",
+        url: "https://my.chenyu.me/qrcode/",
+        icon: "",
+      },
+      {
+        title: "二维码API",
+        description: "生成二维码",
+        url: "https://my.chenyu.me/qrcode/yuqr.php",
+        icon: "",
+      },
+      {
+        title: "屏幕录制",
+        description: "在线屏幕录制",
+        url: "https://my.chenyu.me/record/",
+        icon: "",
+      },
+      {
+        title: "网站监测",
+        description: "关注网站在线情",
+        url: "https://my.chenyu.me/webstatus/",
+        icon: "",
+      },
+      {
+        title: "AI Short",
+        description: "ChatGPT-咒语",
+        url: "https://my.chenyu.me/aishort/",
+        icon: "",
+      },
+      {
+        title: "与佛论禅",
+        description: "听佛解惑",
+        url: "https://my.chenyu.me/buddha/",
+        icon: "",
+      },
+      {
+        title: "HTML加密",
+        description: "给网页加一个密码",
+        url: "https://my.chenyu.me/daima/htmlencrypt/",
         icon: "",
       },
     ],
   },
   {
-    title: "示例分类2",
+    title: "连山",
+      {
+        title: "百度",
+        description: "帮你百度",
+        url: "https://my.chenyu.me/baidu/",
+        icon: "",
+      },
+      {
+        title: "Bing",
+        description: "随机壁纸",
+        url: "https://my.chenyu.me/api/bing/",
+        icon: "",
+      },
+      {
+        title: "毒鸡汤",
+        description: "鸡汤有毒",
+        url: "https://my.chenyu.me/djt/",
+        icon: "",
+      },
+      {
+        title: "舔狗",
+        description: "一无所有",
+        url: "https://my.chenyu.me/tg/",
+        icon: "",
+      },
+      {
+        title: "时钟",
+        description: "漂亮时钟",
+        url: "https://my.chenyu.me/daima/time/",
+        icon: "",
+      },
+      {
+        title: "酷时钟",
+        description: "很酷时钟",
+        url: "https://my.chenyu.me/daima/time/cool/",
+        icon: "",
+      },
+      {
+        title: "单页",
+        description: "漂亮单页",
+        url: "https://my.chenyu.me/daima/good/",
+        icon: "",
+      },
+      {
+        title: "倒计时",
+        description: "人生倒计时",
+        url: "https://my.chenyu.me/daima/rsdjs/",
+        icon: "",
+      },
+      {
+        title: "随机数",
+        description: "随机1-100数",
+        url: "https://my.chenyu.me/daima/sjs/",
+        icon: "",
+      },
+      {
+        title: "喝酒神器",
+        description: "5秒后点击可刷新",
+        url: "https://my.chenyu.me/daima/hjsq/",
+        icon: "",
+      },
+      {
+        title: "尘遇引导页",
+        description: "可选择最短延迟",
+        url: "https://my.chenyu.me/daima/ydy/",
+        icon: "",
+      },
   },
   {
-    title: "示例分类3",
-  },
-  {
-    title: "示例分类4",
-  },
-  {
-    title: "示例分类5",
+    title: "归藏",
+      {
+        title: "俄罗斯方块",
+        description: "小游戏",
+        url: "https://my.chenyu.me/game/tetris/",
+        icon: "",
+      },
+      {
+        title: "小恐龙",
+        description: "恐龙快跑",
+        url: "https://my.chenyu.me/game/xkl/",
+        icon: "",
+      },
+      {
+        title: "魔方",
+        description: "三阶魔方",
+        url: "https://my.chenyu.me/game/cuber/",
+        icon: "",
+      },
+      {
+        title: "圈小猫",
+        description: "小猫快跑",
+        url: "https://my.chenyu.me/game/qxm/",
+        icon: "",
+      },
+      {
+        title: "召唤神龙",
+        description: "正常版",
+        url: "https://my.chenyu.me/game/zhsl/main/",
+        icon: "",
+      },
+      {
+        title: "召唤神龙",
+        description: "无敌版",
+        url: "https://my.chenyu.me/game/zhsl/Invincible/",
+        icon: "",
+      },
+      {
+        title: "召唤神龙",
+        description: "简单版",
+        url: "https://my.chenyu.me/game/zhsl/easy/",
+        icon: "",
+      },
+      {
+        title: "召唤神龙",
+        description: "隐藏红龙版",
+        url: "https://my.chenyu.me/game/zhsl/theHiddenRedDragon/",
+        icon: "",
+      },
+      {
+        title: "青蛙吃蚊子",
+        description: "蚊子快跑",
+        url: "https://my.chenyu.me/game/qwcwz/",
+        icon: "",
+      },
+      {
+        title: "猜数字",
+        description: "猜数字大小",
+        url: "https://my.chenyu.me/game/csz/",
+        icon: "",
+      },
+      {
+        title: "扫雷",
+        description: "经典微软扫雷",
+        url: "https://my.chenyu.me/game/saolei/",
+        icon: "",
+      },
   },
   {
     title: "友情链接",
     sites: [
       {
-        title: "EsunR Blog",
-        description: "EsunR 的博客",
-        url: "https://www.esunr.xyz",
-        icon: "https://esunr-image-bed.oss-cn-beijing.aliyuncs.com/logo.jpg",
+        title: "ChenYu",
+        description: "尘遇",
+        url: "https://chenyu.me",
+        icon: "https://cdn.jsdelivr.net/gh/cy-i/chenyu/logo.png",
       },
     ],
   },
@@ -55,11 +248,11 @@ const GLOBAL_CONFIG = {
   /**
    * 博客名称
    */
-  BLOG_NAME: "EsunR-Blog",
+  BLOG_NAME: "我在等风也等你",
   /**
    * 个人博客链接
    */
-  BLOG_URL: "https://blog.esunr.xyz",
+  BLOG_URL: "https://chenyu.me",
   /**
    * 指定中心 LOGO 图片地址
    */
@@ -67,30 +260,62 @@ const GLOBAL_CONFIG = {
   /**
    * 个人 Github 链接
    */
-  GITHUB_URL: "https://github.com/EsunR",
+  GITHUB_URL: "",
   /**
    * 背景图片地址
    */
-  BACKGROUND_IMG_URL: "https://api.dujin.org/bing/1920.php",
+  BACKGROUND_IMG_URL: "https://my.chenyu.me/random/chenyupic/",
   /**
    * ICP 备案号，留空不显示
    */
-  ICP: "京ICP证000001号",
+  ICP: "沪ICP备15023852号-1",
   ICP_URL: "https://beian.miit.gov.cn/",
   FOOTER_INFO: true,
   /**
    * 网站欢迎标语
    */
   SLOGANS: [
-    "欢迎拜访",
-    "歡迎拜訪",
-    "Welcome, my friend!",
-    "訪問へようこそ",
+    "遇见已是上上签","星光不问赶路人",
+    "众生皆草木，唯你是青山",
+    "时光不负有心人",
+    "幸福，一半争取，一半随缘",
     "嗨，别来无恙",
-    "不忘初心，一生浪漫",
+    "世事茫茫，人生碌碌",
     "最近还好吗？",
     "流星，落花，萤火",
-    "马车越空，晃荡越响",
+    "天上人间，我是风前客",
+    "聚散总是无常",
+    "宁为山水门前客",
+    "一生思念，细水长流",
+    "久处不厌，闲谈不烦",
+    "浮生，往来皆客",
+    "林花谢了春红,太匆匆",
+    "愿山河无恙，人间皆安",
+    "心之所想，皆是过往",
+    "一半烟火，一半清欢",
+    "历尽千帆，归来仍少年",
+    "月圆是画，月缺是诗",
+    "归来是诗，离去是词",
+    "万事随想，所爱如山",
+    "凛冬散尽，星河长明",
+    "红尘的故事叫牵挂",
+    "本是后山人，偶做前堂客",
+    "悲观是远见，乐观是智慧",
+    "光阴者，百代之过客",
+    "言念君子，温其如玉",
+    "小窗闲卧，自醉东篱",
+    "世事无常，聚散终是缘",
+    "一风，一月，一筝，一叹",
+    "途中无所有，摘下一片云",
+    "余生还长，请多指教",
+    "从不敷衍，绝不怠慢",
+    "留人不住，醉解兰舟去",
+    "道不虚行，遇缘即应",
+    "缘起，在人群中看见你",
+    "奔跑的少年，一身清朗",
+    "君记我一瞬，我念君半生",
+    "不雨花犹落，无风絮自飞",
+    "沉默一句句，都是谜题",
   ],
   /**
    * Website 分类配置
@@ -104,9 +329,9 @@ const GLOBAL_CONFIG = {
    * 网站 Title Keywords Description 的配置，用于 SEO
    */
   TKD: {
-    title: "My Blog Index",
-    keywords: "Blog, Index, Index Page",
-    description: "This is my personal blog index page.",
+    title: "尘遇实验室",
+    keywords: "Blog, Index, ChenYu",
+    description: "ChenYu.Me.",
   },
 };
 
